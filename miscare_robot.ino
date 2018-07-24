d22eab24368a493e1d487d9132950e1067d0d2ca
danielmoica16 <41587196+danielmoica16@users.noreply.github.com>
2018-07-24 20:40:16 +0300

    Add files via upload

---
+++ miscare_robot.ino
@@ -0,0 +1,172 @@
+#include <Servo.h>
+#define SERVO_PIN     9  
+#define Echo_PIN    11 
+#define Trig_PIN    12  
+#define BUZZ_PIN     13
+#define SPEED  500     
+int stanga ,centru, dreapta, diagonalastanga, diagonaladreapta;
+const int distantalimita = 27;         
+const int sidedistantalimita = 12; 
+int distanta;
+int num = 0;
+const int intoarcere = 3000; 
+int t;
+Servo head;
+void servo() {
+  head.attach(SERVO_PIN); 
+  head.write(90);
+}
+int MOTOR2_PIN1 = 3;
+int MOTOR2_PIN2 = 5;
+int MOTOR1_PIN1 = 6;
+int MOTOR1_PIN2 = 9;
+void go(int speedLeft, int speedRight) {
+  if (speedLeft > 0) {
+    analogWrite(MOTOR1_PIN1, speedLeft);
+    analogWrite(MOTOR1_PIN2, 0);
+  }
+  else {
+    analogWrite(MOTOR1_PIN1, 0);
+    analogWrite(MOTOR1_PIN2, -speedLeft);
+  }
+
+  if (speedRight > 0) {
+    analogWrite(MOTOR2_PIN1, speedRight);
+    analogWrite(MOTOR2_PIN2, 0);
+  } else {
+    analogWrite(MOTOR2_PIN1, 0);
+    analogWrite(MOTOR2_PIN2, -speedRight);
+  }
+}
+void buzz_ON()  
+{
+  digitalWrite(BUZZ_PIN, LOW);
+}
+void buzz_OFF() 
+{
+  digitalWrite(BUZZ_PIN, HIGH);
+}
+void alarm(){
+   buzz_ON();
+   delay(100);
+   buzz_OFF();
+}
+int observa(){
+  long dep;
+  digitalWrite(Trig_PIN,LOW);
+  delayMicroseconds(5);                                                                              
+  digitalWrite(Trig_PIN,HIGH);
+  delayMicroseconds(15);
+  digitalWrite(Trig_PIN,LOW);
+ dep=pulseIn(Echo_PIN,HIGH);
+  dep=dep*0.01657; 
+  return round(dep);
+}
+
+void imprejurimi(){                     
+  centru= observa();
+  if(centru<distantalimita){
+    alarm();
+    }
+  head.write(120);
+  delay(100);
+ diagonalastanga = observa();                                       
+  if(diagonalastanga<distantalimita){
+go(0,0);
+    alarm();
+    }
+  head.write(170); 
+  delay(300);
+  stanga  = observa();
+  if(stanga <distantalimita){
+go(0,0);
+    alarm();
+    }
+  head.write(120);
+  delay(100);
+  diagonalastanga=observa();
+  if(diagonalastanga<distantalimita){
+   go(0,0);
+    alarm();
+    }
+  head.write(90); 
+  delay(100);
+  centru = observa();
+  if(centru<distantalimita){
+   go(0,0);
+    alarm();
+    }
+  head.write(40);
+  delay(100);
+  diagonaladreapta = observa();
+  if(diagonaladreapta<distantalimita){
+  go(0,0);
+    alarm();
+    }
+  head.write(0);
+  delay(100);
+  dreapta = observa();
+  if(dreapta<sidedistantalimita){
+   go(0,0);
+    alarm();
+    }
+  head.write(90); 
+  delay(300);
+}
+
+void evitare(){
+  go(SPEED,SPEED);
+  ++num;
+  if(num!=0){ 
+  imprejurimi();
+    if(stanga<sidedistantalimita || diagonalastanga<distantalimita){
+      go(SPEED,-SPEED);
+      delay(intoarcere);
+    }
+    if(dreapta<sidedistantalimita || diagonaladreapta<distantalimita){
+      go(-SPEED,SPEED);
+      delay(intoarcere);
+    }
+    if((dreapta<sidedistantalimita || diagonaladreapta<distantalimita)\
+        &&(stanga<sidedistantalimita || diagonalastanga<distantalimita)\
+        &&(centru<distantalimita)){
+          go(-SPEED,-SPEED);
+          delay(2*intoarcere);
+        }
+    num=0; 
+  }
+ distanta = observa(); 
+  if (distanta<distantalimita){ 
+      ++t;}
+  if (distanta>distantalimita){
+      t=0;} 
+  if (t > 25){
+    go(0,0);
+    t=0;
+  }
+}
+
+
+void setup() {
+  pinMode(MOTOR1_PIN1, OUTPUT);
+  pinMode(MOTOR1_PIN2, OUTPUT);
+  pinMode(MOTOR2_PIN1, OUTPUT);
+  pinMode(MOTOR2_PIN2, OUTPUT);
+  pinMode(Trig_PIN, OUTPUT); 
+  pinMode(Echo_PIN,INPUT); 
+  pinMode(BUZZ_PIN, OUTPUT);
+  digitalWrite(BUZZ_PIN, HIGH);  
+  buzz_OFF();
+  digitalWrite(Trig_PIN,LOW);
+  head.attach(SERVO_PIN); 
+  head.write(90);
+  Serial.begin(9600);
+
+}
+
+
+void loop()
+{ evitare();
+imprejurimi();
+observa();
+}
---
+++ senzori_robot.ino
@@ -0,0 +1,100 @@
+ #include<LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+const int temperatura =A2 ;
+int val;
+int vib_pin=7;
+int v;
+int fum = A0;
+int gaz=A1; 
+int sensorThres = 300;             
+const int buzzer = 6; 
+void buzz(){
+ 
+  tone(buzzer, 5000); 
+  delay(1000);        
+  noTone(buzzer);    
+  delay(1000);       
+  
+}
+ 
+void setup()
+{
+    lcd.begin(16,2);
+  Serial.begin(9600);
+  pinMode(A3, INPUT);
+  pinMode(vib_pin,INPUT);
+   pinMode(fum, INPUT);
+   pinMode(gaz, INPUT); 
+     pinMode(temperatura,INPUT);
+
+}
+void loop()
+{  
+//temperatura
+int valoare = analogRead(temperatura);
+  lcd.setCursor(0,1);
+  float m = (valoare / 1024.0) * 5000;
+  float celsius = m/ 100  ;
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura:");
+   lcd.setCursor(0,2);
+  lcd.print(celsius);
+  lcd.print(" C");
+  lcd.setCursor(0,1);
+  delay(1000);
+  //umiditate
+   val = analogRead(A3);
+   Serial.println(val);
+  if(val>100)
+  { 
+     Serial.println(val);
+    buzz();  
+       
+   }
+   else
+   noTone(buzzer);
+ //vibratie
+   int v;
+  v=digitalRead(vib_pin);
+  if(v==1)
+  { 
+     Serial.println(v);
+    delay(1000);
+  buzz();       
+   }
+   else
+  { 
+   noTone(buzzer);
+    delay(1000);
+  }
+   //fum
+   int analogSensor = analogRead(fum);
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+ 
+  if (analogSensor > sensorThres)
+   {
+      Serial.println(analogSensor);
+  buzz(); 
+  }
+   else 
+  noTone(buzzer);
+  
+  delay(1000); 
+  //gaz
+  int senzor = analogRead(gaz);
+  Serial.print("Pin A1: ");
+  Serial.println(senzor);
+ 
+  if (senzor > sensorThres)
+  {
+      Serial.println(senzor);
+       buzz();
+  }
+  else
+   noTone(buzzer);
+  delay(1000);
+  }
+
